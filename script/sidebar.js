13275163c4857ee26e4cc8a19b718bb4ccf9debb
HZY1618yzh <13326945856@sina.cn>
2026-01-03 18:42:27 +0800

    Create sidebar.js

---
+++ script/sidebar.js
@@ -0,0 +1,27 @@
+function initSidebar() {
+    document.getElementById("nav").innerHTML = '<nav class="sidebar">\
+                <ul class="sidebar-nav">\
+                    <li class="nav-item" onclick="window.location=\'https://hzy.us.kg/\'">\
+                        <i class="nav-icon fas fa-home"></i>\
+                        <span class="nav-text">首页</span>\
+                    </li>\
+                    <li class="nav-item" onclick="window.location=\'https://hzy.us.kg/game\'">\
+                        <i class="nav-icon fas fa-gamepad"></i>\
+                        <span class="nav-text">游戏</span>\
+                    </li>\
+                    <li class="nav-item" onclick="window.location=\'https://hzy.us.kg/tools\'">\
+                        <i class="nav-icon fas fa-wrench"></i>\
+                        <span class="nav-text">实用工具</span>\
+                    </li>\
+                    <li class="nav-item" onclick="router.navigate(\'/article\')">\
+                        <i class="nav-icon fas fa-newspaper"></i>\
+                        <span class="nav-text">文章</span>\
+                    </li>\
+                    <li class="nav-divider"></li>\
+                    <li class="nav-item" onclick="window.location=\'https://github.com/HZY1618yzh/home\'">\
+                        <i class="nav-icon fab fa-github"></i>\
+                        <span class="nav-text">GitHub</span>\
+                    </li>\
+                </ul>\
+            </nav>';
+}
