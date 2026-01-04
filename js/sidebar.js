b55e124341521a3ea5d276d68796ef5d1ee930f8
HZY1618yzh <13326945856@sina.cn>
2026-01-04 16:39:28 +0800

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,26 +1,36 @@
 function initSidebar() {
     document.getElementById("nav").innerHTML = '<nav class="sidebar">\
                 <ul class="sidebar-nav">\
-                    <li class="nav-item" onclick="window.location=\'https://hzy.us.kg/\'">\
-                        <i class="nav-icon fas fa-home"></i>\
-                        <span class="nav-text">首页</span>\
+                    <li class="nav-item">\
+                        <a href="/" class="nav-link">\
+                            <i class="nav-icon fas fa-home"></i>\
+                            <span class="nav-text">首页</span>\
+                        </a>\
                     </li>\
-                    <li class="nav-item" onclick="window.location=\'https://hzy.us.kg/game\'">\
-                        <i class="nav-icon fas fa-gamepad"></i>\
-                        <span class="nav-text">游戏</span>\
+                    <li class="nav-item">\
+                        <a href="/game" class="nav-link">\
+                            <i class="nav-icon fas fa-gamepad"></i>\
+                            <span class="nav-text">游戏</span>\
+                        </a>\
                     </li>\
-                    <li class="nav-item" onclick="window.location=\'https://hzy.us.kg/tools\'">\
-                        <i class="nav-icon fas fa-wrench"></i>\
-                        <span class="nav-text">实用工具</span>\
+                    <li class="nav-item">\
+                        <a href="/tools" class="nav-link">\
+                            <i class="nav-icon fas fa-wrench"></i>\
+                            <span class="nav-text">实用工具</span>\
+                        </a>\
                     </li>\
-                    <li class="nav-item" onclick="router.navigate(\'/article\')">\
-                        <i class="nav-icon fas fa-newspaper"></i>\
-                        <span class="nav-text">文章</span>\
+                    <li class="nav-item">\
+                        <a href="/article" class="nav-link">\
+                            <i class="nav-icon fas fa-newspaper"></i>\
+                            <span class="nav-text">文章</span>\
+                        </a>\
                     </li>\
                     <li class="nav-divider"></li>\
-                    <li class="nav-item" onclick="window.location=\'https://github.com/HZY1618yzh/home\'">\
-                        <i class="nav-icon fab fa-github"></i>\
-                        <span class="nav-text">GitHub</span>\
+                    <li class="nav-item">\
+                        <a href="https://github.com/HZY1618yzh/home" class="nav-link" target="_blank">\
+                            <i class="nav-icon fab fa-github"></i>\
+                            <span class="nav-text">GitHub</span>\
+                        </a>\
                     </li>\
                 </ul>\
             </nav>';
