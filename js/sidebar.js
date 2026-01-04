027fcf1a3ac432654d06e5c029b38a17cc7a18f5
HZY1618yzh <13326945856@sina.cn>
2026-01-04 16:54:49 +0800

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,37 +1,43 @@
 function initSidebar() {
     document.getElementById("nav").innerHTML = '<nav class="sidebar">\
                 <ul class="sidebar-nav">\
-                    <li class="nav-item">\
-                        <a href="https://hzy.us.kg/" class="nav-link" title="首页">\
-                            <i class="nav-icon fas fa-home"></i>\
-                            <span class="nav-text">首页</span>\
-                        </a>\
+                    <li class="nav-item" onclick="window.location=\'/\'">\
+                        <i class="nav-icon fas fa-home"></i>\
+                        <span class="nav-text">首页</span>\
                     </li>\
-                    <li class="nav-item">\
-                        <a href="https://hzy.us.kg/game" class="nav-link" title="游戏">\
-                            <i class="nav-icon fas fa-gamepad"></i>\
-                            <span class="nav-text">游戏</span>\
-                        </a>\
+                    <li class="nav-item" onclick="window.location=\'/game\'">\
+                        <i class="nav-icon fas fa-gamepad"></i>\
+                        <span class="nav-text">游戏</span>\
                     </li>\
-                    <li class="nav-item">\
-                        <a href="https://hzy.us.kg/tools" class="nav-link" title="实用工具">\
-                            <i class="nav-icon fas fa-wrench"></i>\
-                            <span class="nav-text">实用工具</span>\
-                        </a>\
+                    <li class="nav-item" onclick="window.location=\'/tools\'">\
+                        <i class="nav-icon fas fa-wrench"></i>\
+                        <span class="nav-text">实用工具</span>\
                     </li>\
-                    <li class="nav-item">\
-                        <a href="#" class="nav-link" title="文章" onclick="event.preventDefault(); router.navigate(\'/article\')">\
-                            <i class="nav-icon fas fa-newspaper"></i>\
-                            <span class="nav-text">文章</span>\
-                        </a>\
+                    <li class="nav-item" onclick="window.location(\'/article\')">\
+                        <i class="nav-icon fas fa-newspaper"></i>\
+                        <span class="nav-text">文章</span>\
                     </li>\
                     <li class="nav-divider"></li>\
-                    <li class="nav-item">\
-                        <a href="https://github.com/HZY1618yzh/home" class="nav-link" title="GitHub" target="_blank" rel="noopener">\
-                            <i class="nav-icon fab fa-github"></i>\
-                            <span class="nav-text">GitHub</span>\
-                        </a>\
+                    <li class="nav-item" onclick="window.location=\'https://github.com/HZY1618yzh/home\'">\
+                        <i class="nav-icon fab fa-github"></i>\
+                        <span class="nav-text">GitHub</span>\
                     </li>\
                 </ul>\
             </nav>';
 }
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        initSidebar();
+    }, 100);
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const navItems = document.querySelectorAll('.nav-item');
+    navItems.forEach(item => {
+        item.setAttribute('tabindex', '0');
+        item.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {
+                this.click();
+            }
+        });
+    });
+});
