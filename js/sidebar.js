117402c1a8d01f552aaaac31dccd5f874b309f04
HZY1618yzh <13326945856@sina.cn>
2026-01-24 19:27:48 +0800

    new sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,51 +1,64 @@
 function initSidebar() {
     document.getElementById("nav").innerHTML = '<nav class="sidebar">\
-                <ul class="sidebar-nav">\
-                    <li class="nav-item" onclick="window.location=\'/\'">\
+                    <a class="nav-item" href=\'/\'>\
                         <i class="nav-icon fas fa-home"></i>\
                         <span class="nav-text">首页</span>\
-                    </li>\
-                    <li class="nav-item" onclick="window.location=\'/game\'">\
+                    </a>\
+                    <a class="nav-item" href=\'/game\'>\
                         <i class="nav-icon fas fa-gamepad"></i>\
                         <span class="nav-text">游戏</span>\
-                    </li>\
-                    <li class="nav-item" onclick="window.location=\'/tools\'">\
+                    </a>\
+                    <a class="nav-item" href=\'/tools\'>\
                         <i class="nav-icon fas fa-wrench"></i>\
                         <span class="nav-text">实用工具</span>\
-                    </li>\
-                    <li class="nav-item" onclick="window.location=\'/article\'">\
+                    </a>\
+                    <a class="nav-item" href=\'/article\'>\
                         <i class="nav-icon fas fa-newspaper"></i>\
                         <span class="nav-text">文章</span>\
-                    </li>\
-                    <li class="nav-item" onclick="window.location=\'/articles\'">\
+                    </a>\
+                    <a class="nav-item" href=\'/articles\'>\
                         <i class="nav-icon fas fa-blog"></i>\
                         <span class="nav-text">静态文章</span>\
-                    </li>\
-                    <li class="nav-item" onclick="window.location=\'/draw\'">\
+                    </a>\
+                    <a class="nav-item" href=\'/draw\'>\
                         <i class="nav-icon fas fa-paintbrush"></i>\
                         <span class="nav-text">冬日绘版</span>\
-                    </li>\
-                    <li class="nav-divider"></li>\
-                    <li class="nav-item" onclick="window.location=\'https://github.com/HZY1618yzh/home\'">\
+                    </a>\
+                    <nav class="nav-divider"></nav>\
+                    <a class="nav-item" href=\'https://github.com/HZY1618yzh/home\'>\
                         <i class="nav-icon fab fa-github"></i>\
                         <span class="nav-text">GitHub</span>\
-                    </li>\
-                    <li class="nav-item" onclick="window.location=\'https://ifdian.net/a/hzy1618yzh/plan\'">\
+                    </a>\
+                    <a class="nav-item" href=\'https://ifdian.net/a/hzy1618yzh/plan\'>\
                         <i class="nav-icon fas fa-hand-holding-usd"></i>\
                         <span class="nav-text">爱发电</span>\
-                    </li>\
-                </ul>\
+                    </a>\
+                    <nav class="nav-divider"></nav>\
+                    <div class="nav-mtitle">链接</div>\
+                    <a class="nav-mtext" href=\'https://oj.hzy.us.kg\'>HZYOJ</a>\
+                    <a class="nav-mtext" href=\'https://oi-wiki.hzy.us.kg\'>OI-wiki</a>\
+                    <a class="nav-mtext" href=\'https://download.hzy.us.kg\'>下载</a>\
+                    <a class="nav-mtext" href=\'https://www.luogu.com.cn/user/1394471\' target="_blank">洛谷主页</a>\
+                    <a class="nav-mtext" href=\'https://github.com/HZY1618yzh\' target="_blank">github主页</a>\
+                    <a class="nav-mtext" href=\'https://www.luogu.com.cn/team/99897\' target="_blank">小狗出题组</a>\
+                    <a class="nav-mtext" href=\'https://blog.csdn.net/D56795?spm=1000.2115.3001.5343\' target="_blank">我的csdn</a>\
+                    <a class="nav-mtext" href=\'mailto: i@hzy.us.kg\'>邮箱</a>\
             </nav>';
 }
 document.addEventListener('DOMContentLoaded', function () {
     initSidebar();
-    const navItems = document.querySelectorAll('.nav-item');
-    navItems.forEach(item => {
-        item.setAttribute('tabindex', '0');
-        item.addEventListener('keypress', function (e) {
-            if (e.key === 'Enter') {
-                this.click();
-            }
-        });
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.addEventListener('mouseleave', function() {
+        setTimeout(() => {
+            this.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }, 300);
     });
+    sidebar.addEventListener('mouseleave', function() {
+        this.scrollTop = 0;
+    });
+    
+    sidebar.style.overflowY = 'overlay';
 });
